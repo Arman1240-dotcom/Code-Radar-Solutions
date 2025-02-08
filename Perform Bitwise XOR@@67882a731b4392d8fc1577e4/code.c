0ce7dcf4dc8fe6780e70edd1dfc41cf35552cdf6
Arman1240-dotcom <arman0047.becse24@chitkara.edu.in>
2025-02-08 21:54:33 +0530

    Solution to Perform Bitwise XOR

---
+++ Perform Bitwise XOR@@67882a731b4392d8fc1577e4/code.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+char* welcome() {
+    return "Welcome to Code Radar!";
+}
+
+int main() {
+    int a,s;
+    scanf("%d %d",&a,&s);
+    printf("%d",a!|s);
+
+    return 0;
+}
