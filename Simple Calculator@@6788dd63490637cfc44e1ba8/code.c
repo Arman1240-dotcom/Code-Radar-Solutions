5eb842e72802461e403951f6c51bdcd7252de4be
Arman Chaudhary <dailybasisgamers@gmail.com>
2025-02-14 12:21:48 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -5,17 +5,21 @@ int main() {
     scanf("%d %d %c",&a,&s,&c);
     switch(c){
         case '+':
-            printf("%d",(a+s));
-            break;
+        printf("%d",(a+s));
+        break;
         case '-':
-            printf("%d",(a-s));
-            break;
+        printf("%d",(a-s));
+        break;
         case '*':
-            printf("%d",(a*s));
-            break;
+        printf("%d",(a*s));
+        break;
         case '/':
-            printf("%d",(a/s));
-            break;
+        if (s != 0) {  // Check for division by zero
+            printf("%d", a / s);
+        } else {
+            printf("Error: Division by zero");
+            }
+        break;  // Exit the switch after the operation
         
         
     }
